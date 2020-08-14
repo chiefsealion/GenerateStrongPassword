1bfe07d943643d32323bd7071d22ecc831213d11
chiefsealion <49793405+chiefsealion@users.noreply.github.com>
2020-08-14 08:57:50 -0700

    Add files via upload

---
+++ Setup-GenerateStrongPassword.ps1
@@ -0,0 +1,9 @@
+﻿New-Item -Path "C:\Program Files\WindowsPowerShell\Modules\" -Name "GenerateStrongPassword" -ItemType Directory
+
+Move-Item -Path "C:\Temp\GenerateStrongPassword.psm1" -Destination "C:\Program Files\WindowsPowerShell\Modules\GenerateStrongPassword\"
+
+Start-Process -FilePath "C:\Temp\AutoHotkey_1.1.33.02_setup.exe"
+
+Copy-Item "C:\Temp\GenerateStrongPassword.ahk" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
+
+Start-Process -FilePath "C:\Temp\GenerateStrongPassword.ahk"
